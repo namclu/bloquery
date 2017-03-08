38febeee798d10f41209099726b9ca41bd5bab5a
namclu <namlu1205@gmail.com>
2017-03-08 12:53:53 +1100

    3 view latest questions: Add question_item.xml. Update QuestionItemAdapter.java onCreateViewHolder(), add references to question.xml view items

--- app/src/main/java/com/namclu/android/bloquery/ui/adapter/QuestionItemAdapter.java
+++ app/src/main/java/com/namclu/android/bloquery/ui/adapter/QuestionItemAdapter.java
@@ -1,10 +1,14 @@
 package com.namclu.android.bloquery.ui.adapter;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.namclu.android.bloquery.R;
 import com.namclu.android.bloquery.api.model.Question;
 
 /**
@@ -18,13 +22,15 @@ public class QuestionItemAdapter extends RecyclerView.Adapter<QuestionItemAdapte
 
 
     @Override
-    public QuestionItemAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public QuestionItemAdapterViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        View inflate = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.question_item, viewGroup, false);
+        return new QuestionItemAdapterViewHolder(inflate);
     }
 
     @Override
     public void onBindViewHolder(QuestionItemAdapterViewHolder holder, int position) {
-
+        
     }
 
     @Override
@@ -35,8 +41,17 @@ public class QuestionItemAdapter extends RecyclerView.Adapter<QuestionItemAdapte
     // Inner class responsible for representing a single View created and returned by an Adapter
     class QuestionItemAdapterViewHolder extends RecyclerView.ViewHolder{
 
+        // Create references to question_item.xml Views
+        TextView question;
+        TextView numberOfAnswers;
+        ImageView userProfile;
+
         public QuestionItemAdapterViewHolder(View itemView) {
             super(itemView);
+
+            question = (TextView) itemView.findViewById(R.id.text_question_item_question);
+            numberOfAnswers = (TextView) itemView.findViewById(R.id.text_question_item_answers);
+            userProfile = (ImageView) itemView.findViewById(R.id.image_question_item_user);
         }
     }
 }
