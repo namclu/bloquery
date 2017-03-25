fd2696f16151d10f7972acecb6398df40340d6e5
namclu <namlu1205@gmail.com>
2017-03-25 17:13:05 +1100

    6: Add textfield and button to submit question in AddQuestionActivity
    Todo: When user hits the 'Add Question' button, add Question to
    Firebase and update the RecyclerView in BloqueryActivity.

--- app/src/main/java/com/namclu/android/bloquery/ui/activity/AddQuestionActivity.java
+++ app/src/main/java/com/namclu/android/bloquery/ui/activity/AddQuestionActivity.java
@@ -2,14 +2,27 @@ package com.namclu.android.bloquery.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.namclu.android.bloquery.R;
 
+import static com.namclu.android.bloquery.R.id.button_add_question;
+
 public class AddQuestionActivity extends AppCompatActivity {
+
+    private EditText mAnswerField;
+    private Button mAnswerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_question);
+
+        // Initialise Database
+
+        // Initialise Views
+        mAnswerField = (EditText) findViewById(R.id.edit_add_question);
+        mAnswerButton = (Button) findViewById(button_add_question);
     }
 }
